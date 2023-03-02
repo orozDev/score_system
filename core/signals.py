768560a546c49e3e3c61621d85cc5bedd5671431
Orozbek <oroz.jenishbek.2003@gmail.com>
2023-03-02 22:10:53 +0600

    ...

---
+++ core/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import pre_delete
+from django.utils import timezone
+from django.dispatch import receiver
+from core.models import Point
+
+@receiver(pre_delete, sender=Point)
+def pre_delete_group(sender, instance, **kwargs):
+    year = int(timezone.now().strftime('%Y'))
+    month = int(timezone.now().strftime('%m'))
+    
+    if year == instance.year and month == instance.month:
+        instance.head.point += instance.value
+        instance.save()
